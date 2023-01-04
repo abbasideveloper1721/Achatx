7f6e896eb9a658f6de4c7b6589a3de1a8ddabd9a
DESKTOP-QNRS06B\dell <majid21va@gmail.com>
2023-01-04 17:48:47 +0500

    intial commit

---
+++ app/src/main/java/com/example/achatx/SignupAct.java
@@ -0,0 +1,14 @@
+package com.example.achatx;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SignupAct extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signup);
+    }
+}
