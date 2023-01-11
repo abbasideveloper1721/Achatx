65537fa66e670800df71a47e75138b484c85c6b0
DESKTOP-QNRS06B\dell <majid21va@gmail.com>
2023-01-11 17:22:08 +0500

    update of signup2

--- app/src/main/java/com/example/achatx/SignupAct.java
+++ app/src/main/java/com/example/achatx/SignupAct.java
@@ -10,5 +10,6 @@ public class SignupAct extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
+        getSupportActionBar().hide();
     }
 }
