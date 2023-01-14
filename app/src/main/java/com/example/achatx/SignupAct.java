42d931d3834a689708a996b2b35b5e3d534982be
DESKTOP-QNRS06B\dell <majid21va@gmail.com>
2023-01-14 22:03:11 +0500

    signin created

--- app/src/main/java/com/example/achatx/SignupAct.java
+++ app/src/main/java/com/example/achatx/SignupAct.java
@@ -4,11 +4,15 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.example.achatx.databinding.ActivitySignupBinding;
+
 public class SignupAct extends AppCompatActivity {
 
+    ActivitySignupBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        binding
         setContentView(R.layout.activity_signup);
         getSupportActionBar().hide();
     }
