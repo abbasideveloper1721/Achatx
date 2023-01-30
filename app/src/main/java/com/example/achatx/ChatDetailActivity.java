39de781c79f8858b6ac66fbee26680bd29d57a5f
DESKTOP-QNRS06B\dell <majid21va@gmail.com>
2023-01-30 09:02:12 +0500

    SHOW MESSAGE IN RECYCLER VIEW 3:58

--- app/src/main/java/com/example/achatx/ChatDetailActivity.java
+++ app/src/main/java/com/example/achatx/ChatDetailActivity.java
@@ -10,11 +10,13 @@ import android.view.View;
 import com.example.achatx.Adapters.ChatAdapter;
 import com.example.achatx.Models.MessagesModel;
 import com.example.achatx.databinding.ActivityChatDetailBinding;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.FirebaseDatabase;
 import com.squareup.picasso.Picasso;
 
 import java.util.ArrayList;
+import java.util.Date;
 
 public class ChatDetailActivity extends AppCompatActivity {
     ActivityChatDetailBinding binding;
@@ -52,6 +54,36 @@ public class ChatDetailActivity extends AppCompatActivity {
         binding.chatRecyclerView.setAdapter(chatAdapter);
         LinearLayoutManager layoutManager = new LinearLayoutManager(this);
         binding.chatRecyclerView.setLayoutManager(layoutManager);
+
+        final String senderRoom = senderId+reciveId;
+        final String reciverRoom = reciveId+senderId;
+
+        binding.sendMsgbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+              String message=  binding.edtextMessage.getText().toString();
+              final MessagesModel model = new MessagesModel(senderId,message);
+              model.setTimestamp(new Date().getTime());
+              binding.edtextMessage.setText("");
+              database.getReference().child("chats")
+                      .child(senderRoom)
+                      .push()
+                      .setValue(model).addOnSuccessListener(new OnSuccessListener<Void>() {
+                          @Override
+                          public void onSuccess(Void unused) {
+                              database.getReference().child("chats")
+                                      .child(reciverRoom)
+                                      .push()
+                                      .setValue(model).addOnSuccessListener(new OnSuccessListener<Void>() {
+                                          @Override
+                                          public void onSuccess(Void unused) {
+
+                                          }
+                                      });
+                          }
+                      });
+            }
+        });
 
 
     }
