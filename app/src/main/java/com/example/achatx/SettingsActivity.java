b926ca51fa7c6fc6dc5d2e52591c6ed0e87301fe
DESKTOP-QNRS06B\dell <majid21va@gmail.com>
2023-02-09 05:28:24 +0500

    image to firbase

--- app/src/main/java/com/example/achatx/SettingsActivity.java
+++ app/src/main/java/com/example/achatx/SettingsActivity.java
@@ -1,14 +1,89 @@
 package com.example.achatx;
 
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import com.example.achatx.databinding.ActivitySettingsBinding;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 
 public class SettingsActivity extends AppCompatActivity {
+
+    ActivitySettingsBinding binding;
+    FirebaseStorage storage;
+
+    ActivityResultLauncher<String> mtakePhoto;
+
+
+
+
+
+
+    FirebaseAuth auth;
+    FirebaseDatabase database;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_settings);
+        binding = ActivitySettingsBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        getSupportActionBar().hide();
+        storage = FirebaseStorage.getInstance();
+        auth = FirebaseAuth.getInstance();
+        database = FirebaseDatabase.getInstance();
+        mtakePhoto = registerForActivityResult(new
+                        ActivityResultContracts.GetContent(),
+                new ActivityResultCallback<Uri>() {
+                    @Override
+                    public void onActivityResult(Uri result) {
+
+                     Uri   imageuri = result;
+
+                        binding.profileimage.setImageURI(result);
+                        final StorageReference reference = storage.getReference().child("profile pictures")
+                                .child(FirebaseAuth.getInstance().getUid());
+                        reference.putFile(imageuri).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                            @Override
+                            public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                                Toast.makeText(SettingsActivity.this,"image uploaded",Toast.LENGTH_SHORT).show();
+                            }
+                        });
+
+
+                    }});
+
+
+        binding.backarrow.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(SettingsActivity.this,MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
+
+
+
+        binding.plus.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mtakePhoto.launch("image/*");
+
+            }
+        });
+
     }
 }
